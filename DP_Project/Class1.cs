18b6a3376ac389c352d6f42ffb4cd8b5a484636f
worso11 <worso@o2.pl>
2021-02-07 14:16:52 +0100

    Added config to connect for DB

--- DP_Project/Class1.cs
+++ DP_Project/Class1.cs
@@ -1,9 +1,25 @@
-﻿namespace DP_Project
+﻿using System.Data.Entity;
+using DP_Project.Migrations;
+
+namespace DP_Project
 {
     public class Class1
     {
         static void Main(string[] args)
         {
+            var context = new ProjectDbContext();
+            context.Database.Initialize(true);
+            var product = new Product()
+            {
+                Name = "Zeszyt",
+                Category = "Szkoła",
+                Discontinued = false
+            };
+            //context.Products.Attach(product);
+            //context.Products.Remove(product);
+            context.Products.Add(product);
+            context.SaveChanges();
+            
             System.Console.WriteLine("Hello World!");
         }
     }
---
+++ DP_Project/Migrations/Configuration.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+
+namespace DP_Project.Migrations
+{
+    internal sealed class Configuration : DbMigrationsConfiguration<DP_Project.ProjectDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+    } 
+}
